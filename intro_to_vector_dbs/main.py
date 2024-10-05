9e307c10328a8f6f7c4a03922ac7652767656894
Miller <miller00315@gmail.com>
2024-10-05 01:10:35 -0300

    ajustadno consultas

---
+++ intro_to_vector_dbs/main.py
@@ -0,0 +1,37 @@
+import os
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain.prompts import PromptTemplate
+from langchain import hub
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.retrieval import create_retrieval_chain
+
+load_dotenv()
+
+os.environ['INDEX_NAME'] = 'medium-blogs-embedding-index'
+
+if __name__ == "__main__":
+    print(" Retrieving...")
+
+    embeddings = OpenAIEmbeddings()
+    llm = ChatOpenAI()
+
+    query = "what is Pinecone in machine learning?"
+    chain = PromptTemplate.from_template(template=query) | llm
+    # result = chain.invoke(input={})
+    # print(result.content)
+
+    vectorstore = PineconeVectorStore(
+        index_name=os.environ["INDEX_NAME"], embedding=embeddings
+    )
+
+    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+    combine_docs_chain = create_stuff_documents_chain(llm, retrieval_qa_chat_prompt)
+    retrival_chain = create_retrieval_chain(
+        retriever=vectorstore.as_retriever(), combine_docs_chain=combine_docs_chain
+    )
+
+    result = retrival_chain.invoke(input={"input": query})
+
+    print(result)
